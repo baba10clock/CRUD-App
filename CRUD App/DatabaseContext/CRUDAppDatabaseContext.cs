6fe43ad87b25145a6eec911fc6b2ffe86b8fba59
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-02-10 21:22:34 +0530

    In memory database setup

---
+++ CRUD App/DatabaseContext/CRUDAppDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_App.DatabaseContext
+{
+    public class CRUDAppDatabaseContext : DbContext
+    {
+        public IConfiguration _config;
+
+        public CRUDAppDatabaseContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("CRUDAppDatabase");
+        }
+    }
+}
